477527b3f2c260e11ca2a375cfd984f715a9134f
NaolDaba <nahafile@gmail.com>
2023-06-25 18:23:28 +0300

    added the createPosts controller logic

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -6,9 +6,20 @@ export const getPosts=async (req, res)=>{
 
         const postMessages = await PostMessage.find();
         res.status(200).json(postMessages);
-
     }catch(error){
         res.status(404).send("unable to create a post");
     }
 }
 
+export const createPosts= async (req, res)=>{
+    const post = req.body;
+    const newPost = new PostMessage(post);
+
+    try{
+        await newPost.save();
+        res.status(201).json(newPost);
+
+    } catch(e){
+        res.status(409).send("failed");
+    }
+}

f54b41ac7a9da26ccbbdd6727c88cb1d8cd1504c
NaolDaba <nahafile@gmail.com>
2023-07-21 21:37:43 +0300

    worked on the redux reducers

---
+++ client/src/reducers/posts.js
@@ -0,0 +1,16 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+export const reducer=createSlice({
+    name: 'posts',
+    initialState:[],
+    reducers:{
+        FETCH_ALL: (state, action) => {return action.payload},
+        CREATE: (state, action) => [...state, action.payload],
+        UPDATE: (state, action) => state.map((post) => post._id === action.payload._id ? action.payload : post ),
+        DELETE: (state, action) => state.filter((post)=>post._id != action.payload),
+        LIKEPOST: (state, action) => state.map((post)=> post._id == action.payload._id ? action.payload : post )
+    }
+})
+
+export const {FETCH_ALL, CREATE, UPDATE, DELETE, LIKEPOST} = reducer.actions;
+export default reducer.reducer;

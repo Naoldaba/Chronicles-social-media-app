ebffec7139638f74e051145d6825b30f957a04f2
NaolDaba <nahafile@gmail.com>
2023-08-02 14:17:38 +0300

    made modifications to the posts reducer

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,10 +1,10 @@
 import {createSlice} from '@reduxjs/toolkit'
 
-export const reducer=createSlice({
+const reducer=createSlice({
     name: 'posts',
     initialState:[],
     reducers:{
-        FETCH_ALL: (state, action) => {return action.payload},
+        FETCH_ALL: (state, action) => action.payload,
         CREATE: (state, action) => [...state, action.payload],
         UPDATE: (state, action) => state.map((post) => post._id === action.payload._id ? action.payload : post ),
         DELETE: (state, action) => state.filter((post)=>post._id != action.payload),

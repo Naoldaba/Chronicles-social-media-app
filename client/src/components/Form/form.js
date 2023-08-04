5a3d2245a8811552f445646982b9548f95586468
NaolDaba <nahafile@gmail.com>
2023-08-04 14:19:58 +0300

    made changes to form component

--- client/src/components/Form/form.js
+++ client/src/components/Form/form.js
@@ -13,8 +13,9 @@ const Form = ({currId, setCurrId})=>{
     const post = useSelector((state)=>currId ? state.posts.find((post)=>post._id==currId): null);
 
     const [postData, setPostData] = useState({
-        creator:"", title:"", message:"", tags:"", selectedFile:""
+        title:"", message:"", tags:"", selectedFile:""
     }) 
+    const user = JSON.parse(localStorage.getItem('profile'))
 
     useEffect(()=>{
         if(post) setPostData(post);
@@ -23,23 +24,32 @@ const Form = ({currId, setCurrId})=>{
     const handleSubmit =(e)=>{
         e.preventDefault();
         if (currId){
-            dispatch(updatePost(currId, postData))
+            dispatch(updatePost(currId, {...postData, name:user?.profileInfo.name}))
         }else{
-            dispatch(createPost(postData));
+            dispatch(createPost({...postData, name:user?.profileInfo.name}));
         }
         clear();
     }
 
     const clear = ()=>{
         setCurrId(null);
-        setPostData({creator:"", title:"", message:"", tags:"", selectedFile:""})
+        setPostData({title:"", message:"", tags:"", selectedFile:""})
+    }
+
+    if (!user?.profileInfo.name){
+        return (
+            <StyledPaper>
+                <Typography variant='h6' align='center' >
+                    Please Sign in to create your own chronicles
+                </Typography>
+            </StyledPaper>
+        )
     }
 
     return (
         <StyledPaper>
             <StyledForm noValidate autoComplete='off' onSubmit={handleSubmit} >
                 <Typography variant='h6'>{currId ? 'Editing' : 'Creating'} a moment</Typography>
-                <TextField name='creator'  variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({...postData, creator: e.target.value})}/>
                 <TextField name='title'  variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({...postData, title: e.target.value})}/>
                 <TextField name='message'  variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e) => setPostData({...postData, message: e.target.value})}/>
                 <TextField name='tags'  variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({...postData, tags: e.target.value.split(',')})}/>

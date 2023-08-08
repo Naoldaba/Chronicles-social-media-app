bf018685ef1f3f4e724901f4c83119ae10d504e6
NaolDaba <nahafile@gmail.com>
2023-08-08 14:22:55 +0300

    updated styles.js

--- client/src/styles.js
+++ client/src/styles.js
@@ -1,22 +1,10 @@
 import { styled } from '@mui/material/styles';
 import {AppBar, Typography, Grid} from '@mui/material';
 
-export const StyledAppBar=styled('div')(({theme})=>({
-    borderRadius: 15,
-    margin: '30px 0',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-}))
-
 export const Heading= styled(Typography)(({theme})=>({
   color:'rgb(0, 183, 255, 1)',
 }))
 
-export const Image=styled('img')(({theme})=>({
-  marginLeft:'15px'
-}))
 
 export const MainContainer = styled(Grid) (({theme})=>({
   [theme.breakpoints.down('sm')]:{

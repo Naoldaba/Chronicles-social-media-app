5a230f1cf4d011ad85f47ffcda3531e6a999e84d
Naol Daba <115622877+Naoldaba@users.noreply.github.com>
2024-06-25 01:13:58 +0300

    Update index.js

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url='http://localhost:5000/posts';
+const url='https://chronicles-backend-orcin.vercel.app';
 
 export const fetchPosts = () => axios.get(url);
 export const makePost = (newPost) => axios.post(url, newPost);

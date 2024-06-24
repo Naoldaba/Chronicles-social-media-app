9749411d1a4c61fa95f338a39e323d88720bd3d2
Naol Daba <115622877+Naoldaba@users.noreply.github.com>
2024-06-25 00:47:12 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,13 +7,7 @@ import dotenv from 'dotenv';
 const app=express();
 dotenv.config();
 
-app.use(cors(
-        {
-           origin: 'https://chronicles-mu.vercel.app/', 
-           methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-           credentials: true, 
-        }
-));
+app.use(cors());
 app.use(express.json({limit: "30mb", extended:true}));
 app.use(express.urlencoded({limit: "30mb", extended:true}));
 

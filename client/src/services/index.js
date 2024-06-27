03cf001bbfeee139b22a5df58728f0862a6054d3
NaolDaba <nahafile@gmail.com>
2024-06-27 16:01:19 +0300

    final touch

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({baseURL : 'chronicles-backend-self.vercel.app'});
+const API = axios.create({baseURL : 'https://chronicles-backend-self.vercel.app'});
 
 API.interceptors.request.use((req)=>{
     if (localStorage.getItem('profile')){

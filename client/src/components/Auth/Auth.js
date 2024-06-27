82321083337e51fcaff065f56a63667a8ee0ffa9
NaolDaba <nahafile@gmail.com>
2024-06-27 15:40:29 +0300

    final touch

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,6 +1,7 @@
+import React, { useState } from 'react';
 import { Grid, Typography, Button } from '@mui/material';
 import LockOutlined from '@mui/icons-material/LockOutlined';
-import { GoogleLogin } from 'react-google-login';
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 import {
   Submit,
   StyledGrid,
@@ -15,7 +16,6 @@ import Icon from './google_icon';
 import { useDispatch } from 'react-redux';
 import { AUTH } from '../../reducers/auth';
 import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
 import { signup, signin } from '../../actions/auth';
 
 const initialState = {
@@ -30,7 +30,6 @@ const Auth = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [formData, setFormData] = useState(initialState);
-
   const [showPassword, setShowPassword] = useState(false);
   const [changeAuthMode, setChangeAuthMode] = useState(false);
   const isSignUp = changeAuthMode;
@@ -53,9 +52,9 @@ const Auth = () => {
     });
   };
 
-  const goSuccess = async (res) => {
-    const profileInfo = res?.profileObj;
-    const token = res?.tokenId;
+  const handleGoogleSuccess = async (response) => {
+    const profileInfo = response.profileObj;
+    const token = response.credential;
     try {
       dispatch(AUTH({ profileInfo, token }));
       navigate('/');
@@ -64,77 +63,76 @@ const Auth = () => {
     }
   };
 
-  const goFailure = () => {
+  const handleGoogleFailure = () => {
     console.log('Google sign in unsuccessful, try again later');
   };
 
   const handleShowPassword = () => setShowPassword((prevState) => !prevState);
 
   return (
-    <StyledContainer maxWidth="sm">
-      <StyledPaper elevation={3}>
-        <StyledAvatar>
-          <LockOutlined />
-        </StyledAvatar>
-        <Typography variant="h5">{isSignUp ? 'Sign Up' : 'Sign In'}</Typography>
-        <SingupForm onSubmit={handleSubmit}>
-          <StyledGrid container spacing={2}>
-            {isSignUp && (
-              <>
-                <CustomInput name="firstName" label="First Name" handleChange={handleChange} half />
-                <CustomInput name="lastName" label="Last Name" handleChange={handleChange} half />
-              </>
-            )}
-            <CustomInput name="email" label="Email Address" handleChange={handleChange} type="email" />
-            <CustomInput
-              name="password"
-              label="Password"
-              handleChange={handleChange}
-              type={showPassword ? 'text' : 'password'}
-              handleShowPassword={handleShowPassword}
-            />
-            {isSignUp && (
+    <GoogleOAuthProvider clientId="745121366812-2mthhe0lppgksip38fqu9ikm7meecf7g.apps.googleusercontent.com">
+      <StyledContainer maxWidth="sm">
+        <StyledPaper elevation={3}>
+          <StyledAvatar>
+            <LockOutlined />
+          </StyledAvatar>
+          <Typography variant="h5">{isSignUp ? 'Sign Up' : 'Sign In'}</Typography>
+          <SingupForm onSubmit={handleSubmit}>
+            <StyledGrid container spacing={2}>
+              {isSignUp && (
+                <>
+                  <CustomInput name="firstName" label="First Name" handleChange={handleChange} half />
+                  <CustomInput name="lastName" label="Last Name" handleChange={handleChange} half />
+                </>
+              )}
+              <CustomInput name="email" label="Email Address" handleChange={handleChange} type="email" />
               <CustomInput
-                name="confirmPassword"
-                label="Repeat Password"
+                name="password"
+                label="Password"
                 handleChange={handleChange}
                 type={showPassword ? 'text' : 'password'}
                 handleShowPassword={handleShowPassword}
               />
-            )}
-          </StyledGrid>
-          <Submit type="submit" fullWidth variant="contained" color="primary">
-            {isSignUp ? 'Sign Up' : 'Sign In'}
-          </Submit>
-          <GoogleLogin
-            clientId="745121366812-2mthhe0lppgksip38fqu9ikm7meecf7g.apps.googleusercontent.com"
-            render={(renderProps) => (
-              <GoogleButton
-                color="primary"
-                fullWidth
-                onClick={renderProps.onClick}
-                disabled={renderProps.disabled}
-                startIcon={<Icon />}
-                variant="contained"
-                disableTouchRipple
-              >
-                Google Login
-              </GoogleButton>
-            )}
-            onSuccess={goSuccess}
-            onFailure={goFailure}
-            cookiePolicy="single_host_origin"
-          />
-          <Grid container justifyContent="flex-end">
-            <Grid item>
-              <Button onClick={changeMode} disableRipple>
-                {isSignUp ? 'Already have an account? Sign In' : "Don't have an account? Sign Up"}
-              </Button>
+              {isSignUp && (
+                <CustomInput
+                  name="confirmPassword"
+                  label="Repeat Password"
+                  handleChange={handleChange}
+                  type={showPassword ? 'text' : 'password'}
+                  handleShowPassword={handleShowPassword}
+                />
+              )}
+            </StyledGrid>
+            <Submit type="submit" fullWidth variant="contained" color="primary">
+              {isSignUp ? 'Sign Up' : 'Sign In'}
+            </Submit>
+            <GoogleLogin
+              onSuccess={handleGoogleSuccess}
+              onFailure={handleGoogleFailure}
+              render={(renderProps) => (
+                <GoogleButton
+                  color="primary"
+                  fullWidth
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                  startIcon={<Icon />}
+                  variant="contained"
+                >
+                  Google Login
+                </GoogleButton>
+              )}
+            />
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Button onClick={changeMode} disableRipple>
+                  {isSignUp ? 'Already have an account? Sign In' : "Don't have an account? Sign Up"}
+                </Button>
+              </Grid>
             </Grid>
-          </Grid>
-        </SingupForm>
-      </StyledPaper>
-    </StyledContainer>
+          </SingupForm>
+        </StyledPaper>
+      </StyledContainer>
+    </GoogleOAuthProvider>
   );
 };
 

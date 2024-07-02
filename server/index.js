5d8ac8f5922cabc74095190ea900323a144c7411
NaolDaba <nahafile@gmail.com>
2024-07-02 14:48:00 +0300

    made modifations

--- server/index.js
+++ server/index.js
@@ -8,7 +8,13 @@ import dotenv from 'dotenv';
 const app=express();
 dotenv.config();
 
-app.use(cors());
+const corsOptions = {
+     origin: 'https://chronicles-pi.vercel.app',
+     methods: ['GET', 'POST', 'OPTIONS'],
+     allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+app.use(cors(corsOptions));
 app.use(express.json({limit: "30mb", extended:true}));
 app.use(express.urlencoded({limit: "30mb", extended:true}));
 
